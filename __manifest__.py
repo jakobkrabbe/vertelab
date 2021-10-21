d40e4cec01f5710a52a422907dad88db28f57c16
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2021-10-21 09:47:56 +0200

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+{
+    "name": "Vertel AB Generic Module",
+    "version": "14.0.1.2.3", 
+    "author": "Vertel AB",
+    "maintainer": "Vertel AB",
+    "repository": "Vertel AB",
+    "license": "AGPL-3",
+    "website": "https://vertel.se/",
+    "category": "Tools",
+    "depends": [
+        'contacts'
+    ],
+    "external_dependencies": [
+    ],
+    "data": [
+        'views/res_partner.xml',
+        'security/ir.model.access.csv' 
+    ],
+    "application": False,
+    "installable": True,
+}
