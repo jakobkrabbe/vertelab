1a13f88689693c3060560255ede214b69076426a
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2021-10-21 09:51:20 +0200

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,21 +1,45 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2021- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
 {
-    "name": "Vertel AB Generic Module",
-    "version": "14.0.1.2.3", 
-    "author": "Vertel AB",
-    "maintainer": "Vertel AB",
-    "repository": "Vertel AB",
-    "license": "AGPL-3",
-    "website": "https://vertel.se/",
-    "category": "Tools",
-    "depends": [
+    'name': 'Vertel AB Generic Module',
+    'version': '14.0.1.2.3',
+    'author': 'Vertel AB',
+    'maintainer': "Vertel AB',
+    'repository': 'https://github.com/jakobkrabbe/vertelab/tree/main',
+    'license': 'AGPL-3',
+    'website': 'https://vertel.se/',
+    'category': 'Tools',
+    'depends': [
         'contacts'
     ],
-    "external_dependencies": [
+    'external_dependencies': [
     ],
-    "data": [
-        'views/res_partner.xml',
-        'security/ir.model.access.csv' 
+    'data': [
+        #'views/res_partner.xml',
+        #'security/ir.model.access.csv' 
     ],
-    "application": False,
-    "installable": True,
+    'application': False,
+    'installable': True,
+    'auto_install': True,
 }
+
+# vim:expandtab:smartindent:tabstop=4s:softtabstop=4:shiftwidth=4:
